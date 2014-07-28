c76ce9888852fab4538a3386739e2334ca44dad1
Christopher Lim <clim@clim-MBP.local>
2014-07-28 11:01:17 -0700

    changed groups from table rows into tabs and works with twitter bootstrap

--- index.php
+++ index.php
@@ -28,7 +28,6 @@ $PASSWORD = $allValues[1];
 $SITE = $allValues[2];
 
 
-
 //*****************************************
 //Get the $USER, $PASSWORD, and $SITE  using a login form at the top of index.php
 //$USER = $_POST['username'];
@@ -37,9 +36,19 @@ $SITE = $allValues[2];
 //*****************************************
 
 
+
+
 //Pulls the xml code and writes it to status.xml in the results subdirectory.
-$xml = file_get_contents("https://$USER:$PASSWORD@$SITE/api/xml");    
-file_put_contents('results/status.xml' , $xml);
+$URL = "https://$USER:$PASSWORD@$SITE/api/xml";
+
+if($xml = file_get_contents($URL))
+{
+	file_put_contents('results/status.xml' , $xml);
+}    
+else
+{
+	exit('Could not get xml');
+}
 
 //Writes the job health html page to display.
 include('pageWriter.php');
